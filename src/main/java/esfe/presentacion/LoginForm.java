c30bacc4cca59f6b5893ab841c7258c098660940
Victoria Reyes <victoriavalencia2006@outlook.com>
2025-06-08 23:10:32 -0600

    Diseño del formulario de login

--- src/main/java/esfe/presentacion/LoginForm.java
+++ src/main/java/esfe/presentacion/LoginForm.java
@@ -3,4 +3,9 @@ package esfe.presentacion;
 import javax.swing.*;
 
 public class LoginForm extends JDialog {
+    private JPanel mainPanel;
+    private JButton btnLogin;
+    private JButton btnSalir;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
 }
