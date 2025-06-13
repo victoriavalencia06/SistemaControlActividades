191a5d600e85daac3443e5d0eb4d4f43b581cb97
Jazmin Rivera <jazminmichellerivera15@outlook.com>
2025-06-12 19:17:20 -0600

    Diseño de formulario de rol.

--- src/main/java/esfe/presentacion/RoleForm.java
+++ src/main/java/esfe/presentacion/RoleForm.java
@@ -3,4 +3,9 @@ package esfe.presentacion;
 import javax.swing.*;
 
 public class RoleForm extends JDialog {
+    private JPanel mainPanel;
+    private JTextField txtNombre;
+    private JComboBox comboEstado;
+    private JButton guardarButton;
+    private JButton cancelarButton;
 }
--- src/main/java/esfe/presentacion/RoleWriteForm.java
+++ src/main/java/esfe/presentacion/RoleWriteForm.java
@@ -3,4 +3,10 @@ package esfe.presentacion;
 import javax.swing.*;
 
 public class RoleWriteForm extends JDialog {
+    private JPanel mainPanel;
+    private JTextField txtNombre;
+    private JButton crearButton;
+    private JTable tableRol;
+    private JButton modificarButton;
+    private JButton eliminarButton;
 }
